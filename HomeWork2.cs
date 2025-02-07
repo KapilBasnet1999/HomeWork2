8aedcaee0d91e17d2dd6effdf5d90a7e173ba2ac
KAPIL BASNET <163415277+KapilBasnet1999@users.noreply.github.com>
2025-02-06 21:03:00 -0600

    Add files via upload

---
+++ HomeWork2.cs
@@ -0,0 +1,168 @@
+﻿// Written by Kapil
+// Feb 5 2025
+
+using System;
+
+namespace HomeWork2
+{
+    public class HomeWork2
+    {
+        // Class variables or member variables
+        private int xCoordinate;
+        private int yCoordinate;
+        private int itemChoice;
+        private string userName;
+        private int targetNumber;
+        private int guessedNumber;
+        private int turn;
+        private int[] originalArray;
+        private int[] copiedArray;
+
+        // Constructor to initialize the values
+        public HomeWork2(int aArraySize = 5, int aXCoordinate = 0, int aYCoordinate = 0, int aItemChoice = -1, string aUserName = "n/a", int aTargetNumber = -1, int aGuessedNumber = -1, int aTurn = 0)
+        {
+            this.xCoordinate = aXCoordinate;
+            this.yCoordinate = aYCoordinate;
+            this.itemChoice = aItemChoice;
+            this.userName = aUserName;
+            this.targetNumber = aTargetNumber;
+            this.guessedNumber = aGuessedNumber;
+            this.turn = aTurn;
+
+            // Initialize arrays with the given size
+            this.originalArray = new int[aArraySize];
+            this.copiedArray = new int[aArraySize];
+        }
+
+        // Properties with custom getters and setters
+        public int XCoordinate
+        {
+            get { return this.xCoordinate; }
+            set { this.xCoordinate = value; }
+        }
+
+        public int YCoordinate
+        {
+            get { return this.yCoordinate; }
+            set { this.yCoordinate = value; }
+        }
+
+        public int ItemChoice
+        {
+            get { return this.itemChoice; }
+            set
+            {
+                if (value >= 1 && value <= 7)
+                    this.itemChoice = value;
+                else
+                    this.itemChoice = -1;  // Invalid item choice
+            }
+        }
+
+        public string UserName
+        {
+            get { return this.userName; }
+            set
+            {
+                // Limiting username length to 16 characters
+                this.userName = value.Length > 16 ? value.Substring(0, 16) : value;
+            }
+        }
+
+        public int TargetNumber
+        {
+            get { return this.targetNumber; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    this.targetNumber = value;
+                else
+                    this.targetNumber = -1; // Invalid target number
+            }
+        }
+
+        public int GuessedNumber
+        {
+            get { return this.guessedNumber; }
+            set { this.guessedNumber = value; }
+        }
+
+        public int Turn
+        {
+            get { return this.turn; }
+            set { this.turn = value; }
+        }
+
+        // Methods
+        // WatchTower
+        public string GetEnemyDirection()
+        {
+            if (xCoordinate == 0 && yCoordinate == 0)
+                return "The enemy is here!";
+            if (xCoordinate > 0 && yCoordinate > 0)
+                return "The enemy is to the northeast!";
+            if (xCoordinate < 0 && yCoordinate > 0)
+                return "The enemy is to the northwest!";
+            if (xCoordinate > 0 && yCoordinate < 0)
+                return "The enemy is to the southeast!";
+            if (xCoordinate < 0 && yCoordinate < 0)
+                return "The enemy is to the southwest!";
+            if (xCoordinate == 0)
+                return (yCoordinate > 0) ? "The enemy is to the north!" : "The enemy is to the south!";
+            return (xCoordinate > 0) ? "The enemy is to the east!" : "The enemy is to the west!";
+        }
+
+        //Buying Inventory
+        public string GetItemPrice()
+        {
+            int price = itemChoice switch
+            {
+                1 => 10,
+                2 => 15,
+                3 => 25,
+                4 => 1,
+                5 => 20,
+                6 => 2,
+                7 => 1,
+                _ => -1
+            };
+            return (price == -1) ? "Invalid item." : $"The item costs {price} gold.";
+        }
+
+        // Discounted Inventory
+        public string GetDiscountedPrice()
+        {
+            int price = itemChoice switch
+            {
+                1 => 10,
+                2 => 15,
+                3 => 25,
+                4 => 1,
+                5 => 20,
+                6 => 2,
+                7 => 1,
+                _ => -1
+            };
+            if (price == -1)
+                return "Invalid item.";
+            if (userName.ToLower() == "kapil")
+                price /= 2;
+            return $"The item costs {price} gold.";
+        }
+
+        // Override the ToString method
+        public override string ToString()
+        {
+            string message = "";
+            message += "Enemy Coordinates: (" + this.XCoordinate + ", " + this.YCoordinate + ")\n";
+            message += "Item Choice: " + this.ItemChoice + "\n";
+            message += "User Name: " + this.UserName + "\n";
+            message += "Target Number: " + this.TargetNumber + "\n";
+            message += "Guessed Number: " + this.GuessedNumber + "\n";
+            message += "Turn: " + this.Turn + "\n";
+            message += "Original Array: " + string.Join(", ", this.originalArray) + "\n";
+            message += "Copied Array: " + string.Join(", ", this.copiedArray) + "\n";
+            return message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿// Written by Kapil
+// Feb 5 2025
+
+using System;
+
+namespace HomeWork2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork2 homework = new HomeWork2();
+
+            Console.WriteLine("Choose a program to run:");
+            Console.WriteLine("1. Watchtower");
+            Console.WriteLine("2. Buying Inventory");
+            Console.WriteLine("3. Discounted Inventory");
+            Console.WriteLine("4. The Prototype");
+            Console.WriteLine("5. The Magic Cannon");
+            Console.WriteLine("6. The Replicator of D'To");
+            Console.Write("Enter your choice (1-6): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter X coordinate: ");
+                    homework.XCoordinate = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Y coordinate: ");
+                    homework.YCoordinate = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(homework.GetEnemyDirection());
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("The following items are available:");
+                    Console.WriteLine("1 - Rope");
+                    Console.WriteLine("2 - Torches");
+                    Console.WriteLine("3 - Climbing Equipment");
+                    Console.WriteLine("4 - Clean Water");
+                    Console.WriteLine("5 - Machete");
+                    Console.WriteLine("6 - Canoe");
+                    Console.WriteLine("7 - Food Supplies");
+                    Console.Write("What number do you want to see the price of? ");
+                    homework.ItemChoice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 3)
+                    {
+                        Console.Write("Enter your name: ");
+                        homework.UserName = Console.ReadLine();
+                        Console.WriteLine(homework.GetDiscountedPrice());
+                    }
+                    else
+                    {
+                        Console.WriteLine(homework.GetItemPrice());
+                    }
+                    break;
+                case 4:// The Prototype
+                    int targetNumber;
+                    do
+                    {
+                        Console.Write("User 1, enter a number between 0 and 100: ");
+                        targetNumber = Convert.ToInt32(Console.ReadLine());
+                    } while (targetNumber < 0 || targetNumber > 100);
+                    Console.Clear();
+                    int guess;
+                    do
+                    {
+                        Console.Write("User 2, guess the number: ");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        if (guess > targetNumber)
+                            Console.WriteLine("Too high!");
+                        else if (guess < targetNumber)
+                            Console.WriteLine("Too low!");
+                    } while (guess != targetNumber);
+                    Console.WriteLine("You guessed the number!");
+                    break;
+                case 5:// The Magic Cannon
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        string blastType = "Normal";
+                        ConsoleColor color = ConsoleColor.Gray; // Default color
+
+                        if (i % 15 == 0)
+                        {
+                            blastType = "Fire & Electric";
+                            color = ConsoleColor.Blue; // Combined blast - Blue
+                        }
+                        else if (i % 3 == 0)
+                        {
+                            blastType = "Fire";
+                            color = ConsoleColor.Red; // Fire - Red
+                        }
+                        else if (i % 5 == 0)
+                        {
+                            blastType = "Electric";
+                            color = ConsoleColor.Yellow; // Electric - Yellow
+                        }
+
+                        Console.ForegroundColor = color; 
+                        Console.WriteLine($"{i}: {blastType}");
+                        Console.ResetColor(); // Reset to default color
+                    }
+                    break;
+
+                case 6:// The Replicator of D'To
+                    int[] originalArray = new int[5];
+                    int[] copiedArray = new int[5];
+                    Console.WriteLine("Enter 5 numbers:");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        originalArray[i] = Convert.ToInt32(Console.ReadLine());
+                        copiedArray[i] = originalArray[i];
+                    }
+                    Console.WriteLine("Original Array: " + string.Join(", ", originalArray));
+                    Console.WriteLine("Copied Array: " + string.Join(", ", copiedArray));
+                    /*int[] originalArray = new int[5];
+                    int[] copiedArray = new int[5];
+
+                    Console.WriteLine("Enter 5 numbers:");
+                     for (int i = 0; i < 5; i++)
+                    {
+                        Console.Write($"Enter number {i + 1}: ");
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            originalArray[i] = number;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a number.");
+                            return; // Or handle the error differently
+                        }
+                    }
+
+                    // Replication (copying)
+                    for (int i = 0; i < 5; i++)
+                    {
+                        copiedArray[i] = originalArray[i];
+                    }
+
+                    Console.WriteLine("Original Array: " + string.Join(", ", originalArray)); // Using string.Join
+                    Console.WriteLine("Copied Array: " + string.Join(", ", copiedArray));     // Using string.Join
+
+                    // Or, if you want to display one element at a time:
+                    //Console.WriteLine("\nOriginal Array (one by one):");
+                    //for (int i = 0; i < 5; i++) {
+                    //    Console.WriteLine(originalArray[i]);
+                    //}
+                    //Console.WriteLine("\nCopied Array (one by one):");
+                    //for (int i = 0; i < 5; i++) {
+                    //    Console.WriteLine(copiedArray[i]);
+                    //} */
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please select a number between 1 and 6.");
+                    break;
+            }
+        }
+    }
+}
